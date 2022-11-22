a01eaca4f964bb707fdbbfc8d96815759009cc4b
Yulia <peresheina.yulia@yandex.ru>
2022-11-22 23:29:14 +0300

    1_lesson/1.0/Program.cs modified

--- 1_Lesson/1.0/Program.cs
+++ 1_Lesson/1.0/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 0. Напишите программу, которая на вход принимает число и
+// выдаёт его квадрат (число умноженное на само себя)
+
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine());
+Console.Write("Квадрат числа равен: ");
+Console.WriteLine(num*num);
