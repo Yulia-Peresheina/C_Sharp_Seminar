8221f80789926d621ad8163c76b06b8bec987a77
Yulia <peresheina.yulia@yandex.ru>
2022-11-26 00:51:52 +0300

    2.1 added

---
+++ 2_lesson/2.1/Program.cs
@@ -0,0 +1,11 @@
+﻿// 1. Напишите программу, которая выводит случайное трёхзначное число
+// и на выходе показывает последнюю цифру этого числа.
+
+
+int LastNumber(int num)
+{
+    Console.WriteLine(num);
+    return num%10;
+}
+
+Console.WriteLine(LastNumber(new Random().Next(100,1000)));
