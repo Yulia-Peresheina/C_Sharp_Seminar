78c374ee957a8ad0a4c5c80bbd028331238ca386
Yulia <peresheina.yulia@yandex.ru>
2022-11-26 01:47:07 +0300

    2.3 added

---
+++ 2_lesson/2.3/Program.cs
@@ -0,0 +1,13 @@
+﻿// 4. Напишите программу, которая принимает на вход число и проверяет,
+// кратно ли оно одновременно 7 и 23.
+
+string Multiple23and7(int num)
+{
+    if (num%7 == 0 && num%23 ==0)
+        return "Multiple";
+   
+    else
+        return "not multiple";
+}
+
+System.Console.WriteLine(Multiple23and7(49));
