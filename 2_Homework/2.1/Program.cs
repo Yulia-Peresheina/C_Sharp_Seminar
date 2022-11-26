5908d13fdcb1499310850b4c65aa2d4bbc28a534
Yulia <peresheina.yulia@yandex.ru>
2022-11-26 16:15:36 +0300

    2.1 Homework

---
+++ 2_Homework/2.1/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число
+//и на выходе показывает вторую цифру этого числа.
+
+int SecNum(int num)
+{
+    Console.WriteLine(num);
+    return (num/10)%10;
+}
+
+Console.WriteLine(SecNum(new Random().Next(100,1000)));
