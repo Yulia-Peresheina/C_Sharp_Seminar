5f8c45864ad6e337ab334c2be96012b282a4cd19
Yulia <peresheina.yulia@yandex.ru>
2022-11-29 16:19:00 +0300

    3.3

---
+++ 3_lesson/3.3/Program.cs
@@ -0,0 +1,28 @@
+﻿// 3. Напишите программу, которая принимает на вход число (N)
+// и выдаёт таблицу квадратов чисел от 1 до N.
+
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    var s = Console.ReadLine();
+    int a = (s == null) ? 0 : int.Parse(s);
+    return a;
+
+}
+
+
+
+void SqrtNum(int num)
+{
+   int i = 1;
+   while (i < (num + 1))
+   {
+        Console.Write(Math.Pow(i, 2)+" ");
+        i++;
+   }
+   return;
+
+}
+
+int N = ReadNumber("Введите число: ");
+SqrtNum(N);
