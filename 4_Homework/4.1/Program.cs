414079a9ca9f7c3952f0e4e7acf066c168455377
Yulia <peresheina.yulia@yandex.ru>
2022-12-03 20:17:48 +0300

    4.1 HW

---
+++ 4_Homework/4.1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B. Без модуля Math, используем цикл for.
+
+int Pow(int num1, int num2)
+{
+    int result = 1;
+    for (int i = 1; i <= num2; i++)
+    {
+        result = result*num1;
+    }
+
+
+    return result;
+}
+
+
+Console.WriteLine("Для возведения числа А в степень В: Введите число А:");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Для возведения числа А в степень В: Введите число B:");
+int B = int.Parse(Console.ReadLine());
+Console.WriteLine(Pow(A, B));
