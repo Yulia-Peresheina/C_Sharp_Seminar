fe5f4b832e06fd74956ea517be43724031da393e
Yulia <peresheina.yulia@yandex.ru>
2022-12-03 09:27:04 +0300

    4.0

---
+++ 4_lesson/4.0/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход число (А)
+// и выдаёт сумму чисел от 1 до А.
+
+
+int Sum(int A)
+{
+    int[] sum = new int[A];
+    Console.WriteLine($"{A} -> ");
+    int result = 0;
+    for (int i = 0; i < sum.Length; i++)
+    {
+        sum[i] = i + 1;
+        result = result + sum[i];
+    }
+    return result;
+}
+
+Console.WriteLine(Sum(15));
+
+// 2 вариант
+int SumNums(int A)
+{
+    int result = 0;
+    for (int i = 0; i <= A; i++)
+    {
+        result += i;
+    }
+    return result;
+}
+Console.WriteLine(SumNums(15));
