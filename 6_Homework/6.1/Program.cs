6b1d09af3f877991a8b0d75c57e1cfd135370898
Yulia <peresheina.yulia@yandex.ru>
2022-12-10 23:27:01 +0300

    6_Homework modified2

--- 6_Homework/6.1/Program.cs
+++ 6_Homework/6.1/Program.cs
@@ -20,20 +20,4 @@ int Vvod()
 Console.WriteLine($"Вы ввели {Vvod()} чисел больше 0.");
 
 
-// Вариант не работает. Почему?
-// int Vvod()
-// {
-//     Console.Write("Введите число: ");
-//     int num = int.Parse(Console.ReadLine()!);
-//     int count = 0;
-//     while((Convert.ToString(num)) != "")
-//     {
-//         if (num > 0)
-//             count += 1;
-//         Console.Write("Введите число: ");
-//         num = int.Parse(Console.ReadLine()!);
-//     }
-//     return count;
-// }
 
-// Console.WriteLine($"Вы ввели {Vvod()} чисел больше 0.");
