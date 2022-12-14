ba63c2a9d18709971b20739380a877050665cf39
Yulia <peresheina.yulia@yandex.ru>
2022-12-14 13:59:58 +0300

    7 Homework edited

--- 7_Homework/7.3/Program.cs
+++ 7_Homework/7.3/Program.cs
@@ -12,36 +12,36 @@ int[,] FillMatrix(int row, int col, int from, int to)
     return matrix;
 }
 
-// int [] AverageCol(int[,] arr)                          //вариант  1. Создаем массив значений. Можно работать дальше.
-// {
-//     int [] averages = new int[arr.GetLength(1)];
-//     for (int i = 0; i < arr.GetLength(1); i++)
-//     {
-//         for (int j = 0; j < arr.GetLength(0); j++)
-//             averages[i] = arr[j, i] + averages[i];
-//     }
-//     return averages;
-// }
-
-void AverageCol(int[,] arr)                          //вариант  2. Просто выводим среднее арифметическое.
+double [] AverageCol(int[,] arr)                          //вариант  1. Создаем массив значений. Можно работать дальше.
 {
+    double [] averages = new double[arr.GetLength(1)];
     for (int i = 0; i < arr.GetLength(1); i++)
     {
-        int averages = 0;
         for (int j = 0; j < arr.GetLength(0); j++)
-            averages = arr[j, i] + averages;
-        Console.WriteLine($"Среднее арифметическое столбца {i + 1} равно {averages}");
+            averages[i] = Math.Round(Convert.ToDouble(arr[j, i])/arr.GetLength(1),2) + averages[i];
     }
+    return averages;
 }
 
-
-// void PrintArray(int[] array)                   //для вывода массива средних арифметических
+// void AverageCol(int[,] arr)                          //вариант  2. Просто выводим среднее арифметическое.
 // {
-//     int n = array.Length;
-//     for (int i = 0; i < n; i++)
-//         Console.Write($" {array[i]} ");
-//     Console.WriteLine();
+//     for (int i = 0; i < arr.GetLength(1); i++)
+//     {
+//         double averages = 0;
+//         for (int j = 0; j < arr.GetLength(0); j++)
+//             averages = arr[j, i] + averages;
+//         Console.WriteLine($"Среднее арифметическое столбца {i + 1} равно {Math.Round(averages/arr.GetLength(1), 2)}");
+//     }
 // }
+
+
+void PrintArray(double[] array)                   //для вывода массива средних арифметических
+{
+    int n = array.Length;
+    for (int i = 0; i < n; i++)
+        Console.Write($" {array[i]} ");
+    Console.WriteLine();
+}
 
 void PrintMatrix(int[,] matr)
 {
@@ -65,6 +65,6 @@ int to = int.Parse(Console.ReadLine()!);
 
 int[,] matrix = FillMatrix(m, n, from, to);
 PrintMatrix(matrix);
-//Console.WriteLine("Среднее арифметическое столбцов: ");
-//PrintArray(AverageCol(matrix));                              //для вывода массива средних арифметических
-AverageCol(matrix);
+Console.WriteLine("Среднее арифметическое столбцов: ");
+PrintArray(AverageCol(matrix));                              //для вывода массива средних арифметических
+// AverageCol(matrix);
