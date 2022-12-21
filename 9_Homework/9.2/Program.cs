9f76abd76fdb97bf9d2ac5915f71786b5eb0c459
Yulia <peresheina.yulia@yandex.ru>
2022-12-21 13:25:26 +0300

    9_Homework and Lesson

---
+++ 9_Homework/9.2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт
+// сумму натуральных элементов в промежутке от M до N.
+
+int SumNumBetween(int M, int N)
+{
+    if (M > N) return 0;
+    return M + SumNumBetween(M + 1, N);
+}
+
+Console.WriteLine(SumNumBetween(int.Parse(Console.ReadLine()!),
+                                int.Parse(Console.ReadLine()!)));
---
+++ 9_lesson/9.1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задайте значенияM и N. Напишите программу, которая рекурсивно выведет 
+//все натуральные числа в промежутке от M до N.
+
+void PrintNumbsBetw(int M, int N)
+{
+    if (M < 0 || N < 0) return;
+    if (M > N) return;
+    Console.Write($"{M} ");
+    PrintNumbsBetw(M + 1, N);
+}
+
+PrintNumbsBetw(15, 30);
